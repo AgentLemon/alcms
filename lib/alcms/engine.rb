860a8c0ba22928e31094ea40a90c8406d1f3f0ef
AgentLemon <omelemon@gmail.com>
2015-11-30 22:16:06 +0400

    Rspec setup.

--- lib/alcms/engine.rb
+++ lib/alcms/engine.rb
@@ -16,6 +16,13 @@ module Alcms
     def self.setup(&block)
       yield self
     end
+
+    config.generators do |g|
+      g.test_framework      :rspec,        fixture: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.assets false
+      g.helper false
+    end
   end
   
 end
