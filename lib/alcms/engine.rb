14e3738524138c0005fd13fd124365c17a48db64
Roman Lugovtsev <omelemon@gmail.com>
2016-01-12 19:10:28 +0400

    Changed middleware insert point.

--- lib/alcms/engine.rb
+++ lib/alcms/engine.rb
@@ -6,7 +6,7 @@ module Alcms
     config.autoload_paths << File.expand_path('../app/renderers', __FILE__)
 
     initializer "static assets" do |app|
-      app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
+      app.middleware.insert_before(::Rack::Lock, ::ActionDispatch::Static, "#{root}/public")
     end
 
     mattr_accessor :editor_mode
--- lib/alcms/version.rb
+++ lib/alcms/version.rb
@@ -1,3 +1,3 @@
 module Alcms
-  VERSION = "0.0.4"
+  VERSION = "0.0.5"
 end
