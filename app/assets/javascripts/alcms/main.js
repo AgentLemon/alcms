28e967df849b87b0ed545210ebcafe519d33a163
Roman Lugovtsev <omelemon@gmail.com>
2017-10-03 11:22:11 +0400

    Separated alcms js initializer to window.Alcms.

--- app/assets/javascripts/alcms/main.js
+++ app/assets/javascripts/alcms/main.js
@@ -8,8 +8,8 @@
 
     CKEDITOR.config.stylesSet = 'alcms_styles:/alcms/styles.js';
 
-    $.fn.initAlcms = function() {
-      $(this).find('.alcms-editable:not(.alcms-readonly)').each(function() {
+    window.Alcms.initCms = function($element) {
+      $element.find('.alcms-editable:not(.alcms-readonly)').each(function() {
         var $this = $(this);
         $this.attr('contenteditable', true);
         CKEDITOR.inline(this).on('change', function() {
@@ -17,9 +17,9 @@
           window.Alcms.unsaved = true;
         });
       });
-    }
+    };
 
-    $body.initAlcms();
+    window.Alcms.initCms($body);
 
     $(window).on('beforeunload', function() {
       return window.Alcms.unsaved ? 'You have unsaved changes!' : undefined;
--- lib/alcms/version.rb
+++ lib/alcms/version.rb
@@ -1,3 +1,3 @@
 module Alcms
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 end
